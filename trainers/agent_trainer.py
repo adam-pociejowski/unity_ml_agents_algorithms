Policy gradients fixed.

trainers/agent_trainer.py
@@ -4,10 +4,11 @@ import abc
 
 
 class AgentTrainer:
-    def __init__(self, brain, brain_name, input_num, output_num, agents_num, model_name, learning_rate=0.0002, epsilon_max=1.0,
-                 epsilon_min=0.01, decay_rate=0.0001, discount_rate=0.9, memory_size=5000, batch_size=32, use_tf=True):
+    def __init__(self, brain, brain_name, input_num, output_num, agents_num, model_name, learning_rate=0.0002,
+                 epsilon_max=1.0, epsilon_min=0.01, decay_rate=0.0001, discount_rate=0.9, memory_size=5000,
+                 batch_size=32, param_decrease_interval=100, epsilon_decay_rate=1.0001, learning_rate_decay_rate=1.0002,
+                 use_tf=True):
         __metaclass__ = abc.ABCMeta
-        print('[' + type(self).__name__ + ' - ' + brain_name + ']:  init')
         self.brain = brain
         self.brain_name = brain_name
         self.input_num = input_num
@@ -22,10 +23,17 @@ class AgentTrainer:
         self.epsilon_min = epsilon_min
         self.decay_rate = decay_rate
         self.epsilon = epsilon_max
+        self.epsilon_decay_rate = epsilon_decay_rate
+        self.param_decrease_interval = param_decrease_interval
+        self.use_tf = use_tf
+        self.current_loss = 0
+        self.step = 0
 
+    def init(self):
+        print('[' + type(self).__name__ + ' - ' + self.brain_name + ']:  init')
         self._init_model()
-        self.summary_writer = tf.summary.FileWriter('summary/' + model_name)
-        if use_tf:
+        self.summary_writer = tf.summary.FileWriter('summary/' + self.model_name)
+        if self.use_tf:
             self.saver = tf.train.Saver()
         self.sess = tf.Session()
         init = tf.global_variables_initializer()
@@ -41,7 +49,9 @@ class AgentTrainer:
         return
 
     def post_step_actions(self, observations, actions, rewards, new_observations):
-        return
+        self.step += 1
+        if self.step % self.param_decrease_interval == 0:
+            self.epsilon = max(self.epsilon_min, self.epsilon_decay_rate**(-1*self.step))
 
     def post_episode_actions(self, rewards, episode):
         self._save_model(None)

trainers/ai_gym.py
@@ -1,7 +1,6 @@
 import gym
-from trainers.algorithms.deep_q_learning_nn_trainer_two_networks import DeepQLearningNNTrainerTwoNetworks
+from trainers.algorithms.policy_gradients_trainer import PolicyGradientsTrainer
 import numpy as np
-import tensorflow as tf
 
 env = gym.make('CartPole-v0')
 env = env.unwrapped
@@ -11,34 +10,27 @@ print(env.observation_space)
 print(env.observation_space.high)
 print(env.observation_space.low)
 
-RENDER_ENV = False
-EPISODES = 500
+render = False
+episodes = 5000
 rewards = []
-RENDER_REWARD_MIN = 800
 total_steps_counter = 0
 
 if __name__ == '__main__':
-    trainer = DeepQLearningNNTrainerTwoNetworks('PPOBrain', input_num=4, output_num=2, agents_num=1,
-                                                memory_size=2000, batch_size=32)
-    for episode in range(400):
+    trainer = PolicyGradientsTrainer(None, 'PPOBrain', input_num=4, output_num=2, layer_1_nodes=10, layer_2_nodes=2,
+                                     agents_num=1, learning_rate=0.01, discount_rate=0.95)
+    trainer.init()
+    for episode in range(episodes):
         observation = env.reset()
         episode_reward = 0
 
         while True:
-            if RENDER_ENV:
+            if render:
                 env.render()
 
             actions = trainer.get_actions([observation])
             new_observation, reward, done, info = env.step(actions[0])
-
-            x, x_dot, theta, theta_dot = new_observation
-            r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
-            r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-            step_reward = r1 + r2
-
-            episode_reward += step_reward
-            trainer.post_step_actions([observation], actions, [step_reward], [new_observation])
-
+            episode_reward += reward
+            trainer.post_step_actions([observation], actions, [reward], [new_observation])
             if done:
                 rewards.append(episode_reward)
                 max_reward_so_far = np.amax(rewards)

trainers/algorithms/deep_q_learning_improved_trainer.py
@@ -1,58 +1,57 @@
 import numpy as np
 import tensorflow as tf
-from trainers.algorithms.agent_trainer import AgentTrainer
+from trainers.agent_trainer import AgentTrainer
 
 
-class DeepQLearningNNTrainerTwoNetworks(AgentTrainer):
+class DeepQLearningImprovedTrainer(AgentTrainer):
 
-    def __init__(self, brain, brain_name, input_num, output_num, agents_num, memory_size=1000, batch_size=32,
-                 layer_1_nodes=10, layer_2_nodes=10):
+    def __init__(self, brain, brain_name, input_num, output_num, agents_num, memory_size=5000, batch_size=32,
+                 layer_1_nodes=128, layer_2_nodes=128, replace_target_iter=100):
         self.layer_1_nodes = layer_1_nodes
         self.layer_2_nodes = layer_2_nodes
-        super().__init__(brain, brain_name, input_num, output_num, agents_num, model_name='deep_q_learning_two_nets',
+        super().__init__(brain, brain_name, input_num, output_num, agents_num, model_name='deep_q_learning_improved',
                          memory_size=memory_size, batch_size=batch_size)
-        self.replace_target_iter = 100
+        self.replace_target_iter = replace_target_iter
         self.learn_step_counter = 0
-        self.memory_counter = 0
-
-    def _init_episode(self):
-        self.action_memory = np.zeros(self.memory_size)
-        self.reward_memory = np.zeros(self.memory_size)
-        self.observation_memory = np.zeros((self.input_num, self.memory_size))
-        self.new_observation_memory = np.zeros((self.input_num, self.memory_size))
 
     def get_actions(self, observation):
-        if np.random.random() > max(self.epsilon, self.epsilon_min):
-            actions_q_value = self.sess.run(self.q_eval_outputs, feed_dict={self.X: np.asarray(observation).transpose()})
-            actions = np.argmax(actions_q_value, axis=0)
+        if np.random.random() > self.epsilon:
+            q_value = self.sess.run(self.Q_value, feed_dict={self.X: np.asarray(observation).transpose()})
+            actions = np.argmax(q_value, axis=0)
         else:
             actions = np.random.randint(self.output_num, size=self.agents_num)
         return actions
 
+    def post_step_actions(self, observations, actions, rewards, new_observations):
+        super().post_step_actions(observations, actions, rewards, new_observations)
+        self._store_memory(observations, actions, rewards, new_observations)
+        if self.episode_step_counter > 2000:
+            self._train()
+
+    def post_episode_actions(self, rewards, episode):
+        self._save_model(None)
+        self._init_episode()
+
     def _store_memory(self, observations, actions, rewards, new_observations):
         for i in range(len(actions)):
-            index = self.memory_counter % self.memory_size
+            index = self.episode_step_counter % self.memory_size
             self.observation_memory[:, index] = observations[i]
             self.action_memory[index] = actions[i]
             self.reward_memory[index] = rewards[i]
             self.new_observation_memory[:, index] = new_observations[i]
-            self.memory_counter += 1
+            self.episode_step_counter += 1
 
     def _replace_target_net_parameters(self):
         t_params = tf.get_collection('target_net_params')
         e_params = tf.get_collection('eval_net_params')
         self.sess.run([tf.assign(t, e) for t, e in zip(t_params, e_params)])
 
-    def post_step_actions(self, observations, actions, rewards, new_observations):
-        self._store_memory(observations, actions, rewards, new_observations)
-        if self.memory_counter > 1000:
-            self._train()
-
     def _train(self):
+        # tf.reset_default_graph()
         if self.learn_step_counter % self.replace_target_iter == 0:
             self._replace_target_net_parameters()
 
-        index_range = min(self.memory_counter, self.memory_size)
+        index_range = min(self.episode_step_counter, self.memory_size)
         sample = np.random.choice(index_range, size=self.batch_size)
 
         observations_sample = self.observation_memory[:, sample]
@@ -60,25 +59,28 @@ class DeepQLearningNNTrainerTwoNetworks(AgentTrainer):
         rewards_sample = self.reward_memory[sample]
         actions_sample = self.action_memory[sample]
 
-        q_next_outputs, q_eval_outputs = self.sess.run([self.q_next_outputs, self.q_eval_outputs], feed_dict={
+        q_target, q_value = self.sess.run([self.Q_target, self.Q_value], feed_dict={
             self.X_: new_observations_sample,
             self.X: observations_sample
         })
 
-        q_target_outputs = q_eval_outputs.copy()
+        q_value_copy = q_value.copy()
         batch_indexes = np.arange(self.batch_size, dtype=np.int32)
         actions_indexes = actions_sample.astype(int)
 
-        q_target_outputs[actions_indexes, batch_indexes] = \
-            rewards_sample + self.discount_rate * np.max(q_next_outputs, axis=0)
-        _, cost = self.sess.run([self.train_op, self.loss], feed_dict={self.X: observations_sample,
-                                                                       self.Y: q_target_outputs})
-        self.epsilon -= self.decay_rate
+        q_value_copy[actions_indexes, batch_indexes] = rewards_sample + self.discount_rate * np.max(q_target, axis=0)
+        _, self.current_loss = self.sess.run([self.train_op, self.loss],
+                                             feed_dict={self.X: observations_sample,
+                                                        self.Y: q_value_copy,
+                                                        self.learning_rate_placeholder: self.learning_rate})
         self.learn_step_counter += 1
 
-    def post_episode_actions(self, rewards, episode):
-        super()._save_model(None)
-        self.learning_rate *= .75
+    def _init_episode(self):
+        self.action_memory = np.zeros(self.memory_size)
+        self.reward_memory = np.zeros(self.memory_size)
+        self.observation_memory = np.zeros((self.input_num, self.memory_size))
+        self.new_observation_memory = np.zeros((self.input_num, self.memory_size))
+        self.episode_step_counter = 0
 
     def _init_model(self):
         W_init = tf.contrib.layers.xavier_initializer(seed=1)
@@ -89,6 +91,7 @@ class DeepQLearningNNTrainerTwoNetworks(AgentTrainer):
     def _build_eval_network(self, layer_1_nodes, layer_2_nodes, W_init, b_init):
         self.X = tf.placeholder(tf.float32, [self.input_num, None], name='s')
         self.Y = tf.placeholder(tf.float32, [self.output_num, None], name='Q_target')
+        self.learning_rate_placeholder = tf.placeholder(tf.float32, [], name='learning_rate')
 
         with tf.variable_scope('eval_net'):
             c_names = ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
@@ -107,12 +110,12 @@ class DeepQLearningNNTrainerTwoNetworks(AgentTrainer):
                 A2 = tf.nn.relu(Z2)
             with tf.variable_scope('layer_3'):
                 Z3 = tf.matmul(W3, A2) + b3
-                self.q_eval_outputs = Z3
+                self.Q_value = Z3
 
         with tf.variable_scope('loss'):
-            self.loss = tf.reduce_mean(tf.squared_difference(self.q_eval_outputs, self.Y))
+            self.loss = tf.reduce_mean(tf.squared_difference(self.Q_value, self.Y))
         with tf.variable_scope('train'):
-            self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+            self.train_op = tf.train.AdamOptimizer(self.learning_rate_placeholder).minimize(self.loss)
 
     def _build_target_network(self, layer_1_nodes, layer_2_nodes, W_init, b_init):
         self.X_ = tf.placeholder(tf.float32, [self.input_num, None], name="s_")
@@ -134,4 +137,4 @@ class DeepQLearningNNTrainerTwoNetworks(AgentTrainer):
                 A2 = tf.nn.relu(Z2)
             with tf.variable_scope('layer_3'):
                 Z3 = tf.matmul(W3, A2) + b3
-                self.q_next_outputs = Z3
+                self.Q_target = Z3

trainers/algorithms/deep_q_learning_trainer.py
@@ -1,36 +1,34 @@
 import numpy as np
 import tensorflow as tf
-from trainers.algorithms.agent_trainer import AgentTrainer
+from trainers.agent_trainer import AgentTrainer
 
 
-class DeepQLearningNNTrainer(AgentTrainer):
+class DeepQLearningTrainer(AgentTrainer):
     def __init__(self, brain, brain_name, input_num, output_num, agents_num, memory_size=5000, batch_size=32,
-                 layer_1_nodes=10, layer_2_nodes=10):
+                 layer_1_nodes=128, layer_2_nodes=128):
         self.layer_1_nodes = layer_1_nodes
         self.layer_2_nodes = layer_2_nodes
         super().__init__(brain, brain_name, input_num, output_num, agents_num, model_name='deep_q_learning',
                          memory_size=memory_size, batch_size=batch_size)
         self.learn_step_counter = 0
 
-    def _init_model(self):
-        W_init = tf.contrib.layers.xavier_initializer(seed=1)
-        b_init = tf.contrib.layers.xavier_initializer(seed=1)
-        self._build_eval_network(self.layer_1_nodes, self.layer_2_nodes, W_init, b_init)
-
     def get_actions(self, observation):
-        if np.random.random() > max(self.epsilon, self.epsilon_min):
-            actions_q_value = self.sess.run(self.q_eval_outputs, feed_dict={self.X: np.asarray(observation).transpose()})
+        if np.random.random() > self.epsilon:
+            actions_q_value = self.sess.run(self.Q, feed_dict={self.X: np.asarray(observation).transpose()})
             actions = np.argmax(actions_q_value, axis=0)
         else:
             actions = np.random.randint(self.output_num, size=self.agents_num)
         return actions
 
-    def _init_episode(self):
-        self.action_memory = np.zeros(self.memory_size)
-        self.reward_memory = np.zeros(self.memory_size)
-        self.observation_memory = np.zeros((self.input_num, self.memory_size))
-        self.new_observation_memory = np.zeros((self.input_num, self.memory_size))
-        self.episode_step_counter = 0
+    def post_step_actions(self, observations, actions, rewards, new_observations):
+        super().post_step_actions(observations, actions, rewards, new_observations)
+        self._store_memory(observations, actions, rewards, new_observations)
+        if self.episode_step_counter > 2000:
+            self._train()
+
+    def post_episode_actions(self, rewards, episode):
+        self._save_model(None)
+        self._init_episode()
 
     def _store_memory(self, observations, actions, rewards, new_observations):
         for i in range(len(actions)):
@@ -41,12 +39,8 @@ class DeepQLearningNNTrainer(AgentTrainer):
             self.new_observation_memory[:, index] = new_observations[i]
             self.episode_step_counter += 1
 
-    def post_step_actions(self, observations, actions, rewards, new_observations):
-        self._store_memory(observations, actions, rewards, new_observations)
-        if self.episode_step_counter > 2000:
-            self._train()
-
     def _train(self):
+        tf.reset_default_graph()
         index_range = min(self.episode_step_counter, self.memory_size)
         sample = np.random.choice(index_range, size=self.batch_size)
 
@@ -55,27 +49,36 @@ class DeepQLearningNNTrainer(AgentTrainer):
         rewards_sample = self.reward_memory[sample]
         actions_sample = self.action_memory[sample]
 
-        q_next_outputs = self.sess.run(self.q_eval_outputs, feed_dict={self.X: new_observations_sample})
-        q_eval_outputs = self.sess.run(self.q_eval_outputs, feed_dict={self.X: observations_sample})
+        q_target = self.sess.run(self.Q, feed_dict={self.X: new_observations_sample})
+        q_value = self.sess.run(self.Q, feed_dict={self.X: observations_sample})
 
-        q_target_outputs = q_eval_outputs.copy()
+        q_value_copy = q_value.copy()
         batch_indexes = np.arange(self.batch_size, dtype=np.int32)
         actions_indexes = actions_sample.astype(int)
 
-        q_target_outputs[actions_indexes, batch_indexes] = \
-            rewards_sample + self.discount_rate * np.max(q_next_outputs, axis=0)
-        _, loss = self.sess.run([self.train_op, self.loss], feed_dict={self.X: observations_sample,
-                                                                       self.Y: q_target_outputs})
-        self.epsilon -= self.decay_rate
+        q_value_copy[actions_indexes, batch_indexes] = rewards_sample + self.discount_rate * np.max(q_target, axis=0)
+        _, self.current_loss = self.sess.run([self.train_op, self.loss],
+                                             feed_dict={self.X: observations_sample,
+                                                        self.Y: q_value_copy,
+                                                        self.learning_rate_placeholder: self.learning_rate})
         self.learn_step_counter += 1
 
-    def post_episode_actions(self, rewards, episode):
-        self._save_model(None)
-        self._init_episode()
+    def _init_episode(self):
+        self.action_memory = np.zeros(self.memory_size)
+        self.reward_memory = np.zeros(self.memory_size)
+        self.observation_memory = np.zeros((self.input_num, self.memory_size))
+        self.new_observation_memory = np.zeros((self.input_num, self.memory_size))
+        self.episode_step_counter = 0
+
+    def _init_model(self):
+        W_init = tf.contrib.layers.xavier_initializer(seed=1)
+        b_init = tf.contrib.layers.xavier_initializer(seed=1)
+        self._build_eval_network(self.layer_1_nodes, self.layer_2_nodes, W_init, b_init)
 
     def _build_eval_network(self, layer_1_nodes, layer_2_nodes, W_init, b_init):
         self.X = tf.placeholder(tf.float32, [self.input_num, None], name='q_learning_s')
         self.Y = tf.placeholder(tf.float32, [self.output_num, None], name='q_learning_Q_target')
+        self.learning_rate_placeholder = tf.placeholder(tf.float32, [], name='q_learning_learning_rate')
 
         with tf.variable_scope('q_learning_eval_net'):
             c_names = ['q_learning_eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
@@ -94,9 +97,9 @@ class DeepQLearningNNTrainer(AgentTrainer):
                 A2 = tf.nn.relu(Z2)
             with tf.variable_scope('layer_3'):
                 Z3 = tf.matmul(W3, A2) + b3
-                self.q_eval_outputs = Z3
+                self.Q = Z3
 
         with tf.variable_scope('q_learning_loss'):
-            self.loss = tf.reduce_mean(tf.squared_difference(self.q_eval_outputs, self.Y))
+            self.loss = tf.reduce_mean(tf.squared_difference(self.Q, self.Y))
         with tf.variable_scope('q_learning_train'):
-            self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+            self.train_op = tf.train.AdamOptimizer(self.learning_rate_placeholder).minimize(self.loss)

trainers/algorithms/policy_gradients_trainer.py
@@ -1,25 +1,23 @@
 import numpy as np
 import tensorflow as tf
-from trainers.algorithms.agent_trainer import AgentTrainer
+from trainers.agent_trainer import AgentTrainer
 
 
-class PolicyGradientTrainer(AgentTrainer):
+class PolicyGradientsTrainer(AgentTrainer):
 
     def __init__(self, brain, brain_name, input_num, output_num, layer_1_nodes, layer_2_nodes, agents_num,
-                 learning_rate=0.001, discount_rate=0.99, batch_size=32, train_interval=1000):
+                 learning_rate=0.0001, discount_rate=0.9, batch_size=32, train_interval=1000, epochs=1):
         self.layer_1_nodes = layer_1_nodes
         self.layer_2_nodes = layer_2_nodes
-        super().__init__(brain, brain_name, input_num, output_num, agents_num, model_name='policy_gradient',
-                         discount_rate=discount_rate, batch_size=batch_size, learning_rate=learning_rate)
+        super().__init__(brain, brain_name, input_num, output_num, agents_num, model_name='policy_gradients',
+                         discount_rate=discount_rate, batch_size=batch_size, learning_rate=learning_rate,
+                         epsilon_max=0.0, epsilon_min=0.0)
         self.train_interval = train_interval
 
     def _init_episode(self):
-        self.step_counter = 0
         self.episode_observations, self.episode_actions, self.episode_rewards = [], [], []
-        for i in range(self.agents_num):
-            self.episode_observations.append([])
-            self.episode_actions.append([])
-            self.episode_rewards.append([])
+        self.train_interval_counter = 0
+        self.selected_actions = np.zeros(self.output_num, dtype=int)
 
     def get_actions(self, observation):
         actions = []
@@ -31,50 +29,44 @@ class PolicyGradientTrainer(AgentTrainer):
         return actions
 
     def _store_memory(self, observation, actions, reward):
+        # if self.step % 500 == 0:
+        #     print('selected actions: {}'.format(self.selected_actions))
+        #     self.selected_actions = np.zeros(self.output_num, dtype=int)
+
         for i in range(self.agents_num):
             action_vector = np.zeros(self.output_num)
             action_vector[int(actions[i])] = 1
-            self.episode_actions[i].append(action_vector)
-            self.episode_observations[i].append(observation[i])
-            self.episode_rewards[i].append(reward[i])
-            if reward[i] > 0.5:
-                print('reward: {}', reward[i])
-                self._train(i)
+            self.selected_actions[int(actions[i])] += 1
+            self.episode_actions.append(action_vector)
+            self.episode_observations.append(observation[i])
+            self.episode_rewards.append(reward[i])
 
     def post_step_actions(self, observations, actions, rewards, new_observations):
+        super().post_step_actions(observations, actions, rewards, new_observations)
         self._store_memory(observations, actions, rewards)
-        self.step_counter += 1
-        # if self.step_counter % self.train_interval == 0:
-        #     self._train()
 
-    def _train(self, agent_index):
-        discounted_episode_rewards_norm = self.discount_and_norm_rewards(self.episode_rewards[agent_index])
-        print('[train] index: {}, len: {}'.format(agent_index, len(self.episode_rewards[agent_index])))
-        self.sess.run(self.train_op, feed_dict={
-            self.X: np.vstack(self.episode_observations[agent_index]).T,
-            self.Y: np.vstack(np.array(self.episode_actions[agent_index])).T,
-            self.discounted_episode_rewards_norm: discounted_episode_rewards_norm,
-        })
-        self.episode_rewards[agent_index] = []
-        self.episode_actions[agent_index] = []
-        self.episode_observations[agent_index] = []
+    def _train(self):
+        discounted_episode_rewards = self.discount_and_norm_rewards()
+        _, self.current_loss = self.sess.run([self.train_op, self.loss],
+                                             feed_dict={self.X: np.vstack(self.episode_observations).T,
+                                                        self.Y: np.vstack(np.array(self.episode_actions)).T,
+                                                        self.discounted_episode_rewards_norm: discounted_episode_rewards
+                                                        })
 
-    def discount_and_norm_rewards(self, agent_episode_rewards):
-        discounted_episode_rewards = np.zeros_like(agent_episode_rewards)
+    def discount_and_norm_rewards(self):
+        discounted_episode_rewards = np.zeros_like(self.episode_rewards)
         cumulative_reward = 0
-        for i in reversed(range(len(agent_episode_rewards))):
-            cumulative_reward = cumulative_reward * self.discount_rate + agent_episode_rewards[i]
+        for i in reversed(range(len(self.episode_rewards))):
+            cumulative_reward = cumulative_reward * self.discount_rate + self.episode_rewards[i]
             discounted_episode_rewards[i] = cumulative_reward
 
         discounted_episode_rewards -= np.mean(discounted_episode_rewards)
-        std_discounted_episode_rewards = np.std(discounted_episode_rewards)
-        if std_discounted_episode_rewards != 0.0:
-            discounted_episode_rewards /= std_discounted_episode_rewards
+        discounted_episode_rewards /= np.std(discounted_episode_rewards)
         return discounted_episode_rewards
 
     def post_episode_actions(self, rewards, episode):
-        self._save_model(None)
-        self.learning_rate *= .8
+        self._train()
+        self._init_episode()
 
     def _init_model(self):
         with tf.name_scope('inputs'):
@@ -107,6 +99,6 @@ class PolicyGradientTrainer(AgentTrainer):
         self.outputs_softmax = tf.nn.softmax(logits, name='A3')
         with tf.name_scope('loss'):
             neg_log_prob = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
-            loss = tf.reduce_mean(neg_log_prob * self.discounted_episode_rewards_norm)
+            self.loss = tf.reduce_mean(neg_log_prob * self.discounted_episode_rewards_norm)
         with tf.name_scope('train'):
-            self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
+            self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)

trainers/trainer_gym.py
@@ -1,20 +1,21 @@
-from trainers.algorithms.genetic_algorithm_trainer import GeneticAlgorithmTrainer
-from gym_unity.envs import UnityEnv
 import numpy as np
+from gym_unity.envs import UnityEnv
+from trainers.algorithms.deep_q_learning_trainer import DeepQLearningTrainer
 
 number_of_agents = 40
 number_of_episodes = 20
 episode_max_length = 500
 
 
-def run_episode(env, trainer):
-    observation = env.reset()
+def run_episode(_env, _trainer):
+    observation = _env.reset()
     total_rewards = [0.0] * number_of_agents
-    # Running episode for every agent
     for t in range(episode_max_length):
-        actions = trainer.get_actions(observation)
-        observation, reward, done, info = env.step(actions)
+        actions = _trainer.get_actions(observation)
+        new_observation, reward, done, info = _env.step(actions)
+        _trainer.post_step_actions(observation, actions, reward, new_observation)
         total_rewards = [sum(x) for x in zip(total_rewards, reward)]
+        observation = new_observation
 
     return total_rewards
 
@@ -23,11 +24,13 @@ if __name__ == '__main__':
     print('main')
     env_name = "../../env/multi-6-agent/Tanks"
     env = UnityEnv(env_name, worker_id=0, use_visual=False, multiagent=True)
-    trainer = GeneticAlgorithmTrainer('PPOBrain', number_of_observations=42, number_of_actions=6,
-                                      number_of_chromosomes=40, elite_chromosomes=8, hidden_layer_nodes=128)
+    trainer = DeepQLearningTrainer(
+                        None, 'PPOBrain', input_num=87, output_num=6, agents_num=64, memory_size=5000, batch_size=32,
+                        layer_1_nodes=128, layer_2_nodes=128)
+
     for episode in range(number_of_episodes):
         rewards = run_episode(env, trainer)
-        trainer.update_model(rewards)
+        trainer.post_episode_actions(rewards, episode)
         best = np.amax(rewards)
         avg = np.average(rewards)
         print("Episode: {}, Avg: {}, Best: {}".format(episode + 1, avg, best))

trainers/trainer_python_api_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from operator import add
 import tensorflow as tf
+from trainers.trainer_python_api import EPISODE_LENGTH, LOG_INTERVAL
 
 
 def update_rewards(rewards, trainers, new_env_info, actions, env_info):
@@ -38,7 +39,7 @@ def run_episode(trainers, env, episode, train_mode, episode_max_length):
         actions = get_actions(trainers, env_info)
         new_env_info = env.step(actions)
         rewards = update_rewards(rewards, trainers, new_env_info, actions, env_info)
-        if step % 1000 == 0 and step > 0:
+        if step % LOG_INTERVAL == 0 and step > 0:
             rewards_log_copy = log_stats(trainers, rewards_log_copy, rewards, episode, step)
         env_info = new_env_info
         
@@ -53,7 +54,6 @@ def post_episode_actions(trainers, rewards, episode):
         best = np.amax(model_rewards)
         avg = np.average(model_rewards)
         print("[BRAIN]:       {:8}, avg: {:8.4f}, max: {:8.4f}".format(t.brain_name, avg, best))
-        save_summary(t, avg, best, (episode+1)*5000)
     print("\n")
 
 
@@ -64,16 +64,17 @@ def log_stats(trainers, rewards_log_copy, rewards, episode, step):
         best = np.amax(rewards_change)
         avg = np.average(rewards_change)
         print("[BRAIN]:       {:8}, avg: {:8.4f}, best: {:8.4f}".format(t.brain_name, avg, best))
-        save_summary(t, avg, best, episode*5000 + step)
+        save_summary(t, avg, best, episode*(EPISODE_LENGTH - 1) + step)
     return rewards.copy()
 
 
 def save_summary(trainer, avg_reward, best_reward, step):
-    summary, _, _, _, _ = sess.run([merged, mean_reward, max_reward, learning_rate, random_action_chance],
-                                   feed_dict={mean_reward_placeholder: avg_reward,
-                                              max_reward_placeholder: best_reward,
-                                              learning_rate_placeholder: trainer.learning_rate,
-                                              random_action_chance_placeholder: trainer.epsilon})
+    summary, _, _, _, _, _ = sess.run([merged, mean_reward, max_reward, learning_rate, random_action_chance, loss],
+                                      feed_dict={mean_reward_placeholder: avg_reward,
+                                                 max_reward_placeholder: best_reward,
+                                                 loss_placeholder: trainer.current_loss,
+                                                 learning_rate_placeholder: trainer.learning_rate,
+                                                 random_action_chance_placeholder: trainer.epsilon})
     trainer.summary_writer.add_summary(summary, step)
 
 
@@ -92,4 +93,7 @@ with tf.name_scope('Summaries'):
 
     random_action_chance_placeholder = tf.placeholder(tf.float32, shape=None, name='random_action_chance')
     random_action_chance = tf.summary.scalar('random_action_chance', random_action_chance_placeholder)
+
+    loss_placeholder = tf.placeholder(tf.float32, shape=None, name='loss')
+    loss = tf.summary.scalar('loss', loss_placeholder)
     merged = tf.summary.merge_all()